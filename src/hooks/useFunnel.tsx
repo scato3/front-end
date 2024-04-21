d3e407291788f323d09a045e1648dbdf9f6caa89
shuding0307 <suhyun2116@gmail.com>
2024-04-21 21:45:04 +0900

    fix: useFunnel 훅 수정

--- src/hooks/useFunnel.tsx
+++ src/hooks/useFunnel.tsx
@@ -2,27 +2,28 @@
 
 import { ReactElement, ReactNode, useState } from "react";
 
-export interface Step {
+export interface StepProps {
   name: string;
   children: ReactNode;
 }
 
-export interface Funnel {
-  children: Array<ReactElement<Step>>;
+export interface FunnelProps {
+  children: Array<ReactElement<StepProps>>;
 }
 
 export const useFunnel = <T extends string>(defaultStep: T) => {
   const [step, setStep] = useState(defaultStep);
 
-  const Step = (props: Step): ReactElement => {
+  const Step = (props: StepProps): ReactElement => {
     return <>{props.children}</>;
   };
 
-  const FunnelRoot = ({ children }: Funnel) => {
+  const Funnel = ({ children }: FunnelProps) => {
     const targetStep = children.find((childStep) => childStep.props.name === step);
     return <>{targetStep}</>;
   };
-  const Funnel = Object.assign(FunnelRoot, { Step });
 
-  return [Funnel, setStep] as const;
+  return [Funnel, Step, setStep] as const;
 };
+
+export default useFunnel;
