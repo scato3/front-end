d4d20d058aa88cd73d8f0d458d35aa8c6244fd7e
shuding0307 <suhyun2116@gmail.com>
2024-04-29 17:42:05 +0900

    button modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../styles/globals.css";
 import RQProvider from "./_component/RQProvider";
 import { Suspense } from "react";
 
