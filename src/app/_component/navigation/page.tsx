3dedb00f10deb55fd16c29ab254c422d95465226
hs <tlsgustn93@naver.com>
2024-05-02 19:53:21 +0900

    feat: 색상 추가

--- src/app/_component/navigation/page.tsx
+++ src/app/_component/navigation/page.tsx
@@ -3,15 +3,21 @@ import Arrow from "../../../../public/icons/Btn_arrow.svg";
 import Search from "../../../../public/icons/Icon_search.svg";
 import Image from "next/image";
 
-interface INavigaionProps {
+interface INavigationProps {
   children: React.ReactNode;
   onClick: () => void;
   isSearch?: boolean;
 }
 
-export default function Navigation({ children, onClick, isSearch }: INavigaionProps) {
+export default function Navigation({ children, onClick, isSearch }: INavigationProps) {
   return (
-    <div className={styles.Container}>
+    <div
+      className={styles.Container}
+      style={{
+        background: isSearch ? "var(--bg-default)" : "var(--gray-800)",
+        color: isSearch ? "var(--gray-800)" : "var(--gray-200)",
+      }}
+    >
       <div className={styles.BackButton}>
         <Image src={Arrow} alt="뒤로가기 버튼" width={48} height={48} onClick={onClick} />
       </div>
