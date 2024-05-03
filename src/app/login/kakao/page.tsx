13aa3e1d18eaf7b5f5a3b573cc67368eac4102ce
minjamie <minjae2246@gamil.com>
2024-05-03 16:42:23 +0900

    feat: kakao 로그인 후 회원가입 여부에 따라 메인과 회원가입 분기

--- src/app/login/kakao/page.tsx
+++ src/app/login/kakao/page.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-import { useEffect } from "react";
+import getKakaoCode from "@/app/api/kakao";
 import useAuth from "@/hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
-import getKakaoCode from "@/app/api/kakao";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Kakao() {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
   const { setUserData } = useAuth();
-
+  
   const { data } = useQuery({
     queryKey: ["KAKAO_CODE", code],
     queryFn: async () => getKakaoCode(code),
   });
+  const router = useRouter();
+
 
   useEffect(() => {
     if (data) {
       setUserData(data.accessToken, true);
+      data?.joinDate ? router.push("/home") : router.push("/setProfile");
     }
   }, [data]);
 }
---
+++ src/app/main/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Main() {
+  return (
+    <div>Main이지렁</div>
+  )
+}
