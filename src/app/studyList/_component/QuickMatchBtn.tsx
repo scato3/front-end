b2655c072734895a4109646aaf621d9f8810329f
hs <tlsgustn93@naver.com>
2024-05-09 17:48:55 +0900

    fix: Modal 에러 수정

--- src/app/studyList/_component/QuickMatchBtn.tsx
+++ src/app/studyList/_component/QuickMatchBtn.tsx
@@ -7,46 +7,51 @@ import Image from "next/image";
 import ModalContainer from "@/app/_component/ModalContainer";
 import ModalPortal from "@/app/_component/ModalPortal";
 import QuickModal from "./QuickModal";
+import { useModal } from "@/hooks/useModal";
 
 export default function QuickMatchBtn() {
-    const [ quickMatch, setQuickMatch ] = useState<boolean>(false);
-    const [ isModalOpen, setIsModalOpen ] = useState(false);
-    const [ isFiltered, setIsFiltered ] = useState();
+  const [quickMatch, setQuickMatch] = useState<boolean>(false);
+  const { openModal, handleOpenModal, handleCloseModal } = useModal();
 
-    const toggleModal = () => {
-        setIsModalOpen(!isModalOpen);
-        setQuickMatch(false);
-    }
+  const [isFiltered, setIsFiltered] = useState();
 
-    const handleQuickMatch = () => {
-        setQuickMatch(true);
-        setIsModalOpen(true);
-    };
+  const toggleModal = () => {
+    setQuickMatch(false);
+    handleCloseModal();
+  };
 
-    return (
-        <div className={styles.container}>
-            {quickMatch && (
-                <button className={styles.resetBtn}>
-                    <Image src={resetIcon} width={20} height={20} alt="reset" />
-                </button>
-            )}
-            <button className={quickMatch ? `${styles.quickMatchBtn} ${styles.quickMatchBtnActive}` : styles.quickMatchBtn}
-                    onClick={handleQuickMatch}>
-                <Image 
-                    className={styles.quickIcon} 
-                    src={quickMatch ? Icon_quickActive : Icon_quick} 
-                    width={20} 
-                    height={20} 
-                    alt="quick" />
-                빠른매칭
-            </button>
-            {isModalOpen&& (
-                <ModalPortal>
-                    <ModalContainer handleCloseModal={toggleModal}>
-                        <QuickModal setIsModalOpen={setIsModalOpen} setQuickMatch={setQuickMatch} />
-                    </ModalContainer>
-                </ModalPortal>
-            )}
-        </div>
-    );
+  const handleQuickMatch = () => {
+    setQuickMatch(true);
+    handleOpenModal();
+  };
+
+  return (
+    <div className={styles.container}>
+      {quickMatch && (
+        <button className={styles.resetBtn}>
+          <Image src={resetIcon} width={20} height={20} alt="reset" />
+        </button>
+      )}
+      <button
+        className={quickMatch ? `${styles.quickMatchBtn} ${styles.quickMatchBtnActive}` : styles.quickMatchBtn}
+        onClick={handleQuickMatch}
+      >
+        <Image
+          className={styles.quickIcon}
+          src={quickMatch ? Icon_quickActive : Icon_quick}
+          width={20}
+          height={20}
+          alt="quick"
+        />
+        빠른매칭
+      </button>
+      {openModal && (
+        <ModalPortal>
+          <ModalContainer handleCloseModal={toggleModal}>
+            <QuickModal handleCloseModal={handleCloseModal} setQuickMatch={setQuickMatch} />
+          </ModalContainer>
+        </ModalPortal>
+      )}
+    </div>
+  );
 }
