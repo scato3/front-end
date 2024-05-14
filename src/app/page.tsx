2a6ba2739e1be1a3d2fff124800bb340ff7ca672
hs <tlsgustn93@naver.com>
2024-05-14 15:22:13 +0900

    style: style 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,5 @@
-"use client";
-
-import Input_Chat from "./_component/input/Input_Chat";
-import Footer from "./_component/footer/footer";
-import Toggle from "./_component/toggle/toggle";
-import Navigation from "./_component/navigation/page";
-import Guide from "./_component/main_home/Guide";
-import ButtonBox from "./_component/main_home/ButtonBox";
+import Main_home from "./home/page";
 
 export default function Home() {
-  return (
-    <div>
-      <br />
-      <Input_Chat />
-      <br />
-      <Footer />
-      <Footer selectedIndex={1} />
-      <Footer selectedIndex={2} />
-      <Footer selectedIndex={3} />
-      <br />
-      <Toggle />
-      <br />
-      <Navigation dark={true} onClick={() => {}} isSearch={true}>
-        프로필 등록
-      </Navigation>
-      <hr />
-      <Guide
-          onClick={() => {}}>
-            <p>쇼터디 가입하고</p>
-            <p>나에게 딱맞는 스터디 찾아요</p>
-      </Guide>
-      <hr />
-      <br />
-      <ButtonBox />
-      <hr />
-    </div>
-  );
+  return <Main_home />;
 }
