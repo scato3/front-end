a19204fb64b3679fea887d3b2a40041f4fe1355c
hs <tlsgustn93@naver.com>
2024-05-16 02:18:30 +0900

    refactor: 로컬스토리지로 변경

--- src/app/fastMatching/store/FastStore.ts
+++ src/app/fastMatching/store/FastStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface useFastStore {
   selectedDate: string | null;
@@ -19,31 +20,39 @@ interface useFastStore {
   setSave: (value: boolean) => void;
 }
 
-const useFastStore = create<useFastStore>((set) => ({
-  selectedDate: null,
-  setSelectedDate: (date: string | null) => set({ selectedDate: date }),
-  selectedField: null,
-  setSelectedField: (field: string | null) => set({ selectedField: field }),
-  selectedDuration: null,
-  setSelectedDuration: (duration: string | null) => set({ selectedDuration: duration }),
-  recruit: 2,
-  setRecruit: (value: number) => set({ recruit: value }),
-  recruitArr: [],
-  setRecruitArr: (arrUpdater: (prev: number[]) => number[]) => {
-    set((state) => ({
-      recruitArr: arrUpdater(state.recruitArr),
-    }));
-  },
-  tendency: [],
-  setTendency: (arrUpdater: (prev: string[]) => string[]) => {
-    set((state) => ({
-      tendency: arrUpdater(state.tendency),
-    }));
-  },
-  matchingType: null,
-  setMatchingType: (matchingType: string | null) => set({ matchingType: matchingType }),
-  save: false,
-  setSave: (save: boolean) => set({ save: save }),
-}));
+const useFastStore = create<useFastStore>(
+  persist<useFastStore>(
+    (set) => ({
+      selectedDate: null,
+      setSelectedDate: (date: string | null) => set({ selectedDate: date }),
+      selectedField: null,
+      setSelectedField: (field: string | null) => set({ selectedField: field }),
+      selectedDuration: null,
+      setSelectedDuration: (duration: string | null) => set({ selectedDuration: duration }),
+      recruit: 2,
+      setRecruit: (value: number) => set({ recruit: value }),
+      recruitArr: [],
+      setRecruitArr: (arrUpdater: (prev: number[]) => number[]) => {
+        set((state) => ({
+          recruitArr: arrUpdater(state.recruitArr),
+        }));
+      },
+      tendency: [],
+      setTendency: (arrUpdater: (prev: string[]) => string[]) => {
+        set((state) => ({
+          tendency: arrUpdater(state.tendency),
+        }));
+      },
+      matchingType: null,
+      setMatchingType: (matchingType: string | null) => set({ matchingType: matchingType }),
+      save: false,
+      setSave: (save: boolean) => set({ save: save }),
+    }),
+    {
+      name: "userFilter",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ) as any,
+);
 
 export default useFastStore;
