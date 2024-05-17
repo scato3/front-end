d54a15f4e5e099c0ff9e2fa37e48c55507666edb
hs <tlsgustn93@naver.com>
2024-05-18 03:23:42 +0900

    fix: 데이터 남아있는거 수정

--- src/app/createStudy/_component/CreateModal.tsx
+++ src/app/createStudy/_component/CreateModal.tsx
@@ -1,14 +1,18 @@
 import styles from "./create.module.css";
 import Button from "@/app/_component/button/Button";
 import { useRouter } from "next/navigation";
+import useCreateStore from "../store/CreateStore";
 
 interface ICreateModal {
   handleCloseModal: () => void;
 }
 
 export default function CreateModal({ handleCloseModal }: ICreateModal) {
+  const { setSelectedDuration, setSelectedDate } = useCreateStore();
   const router = useRouter();
   const handleCreate = () => {
+    setSelectedDuration(null);
+    setSelectedDate(null);
     router.push("./home");
     handleCloseModal();
   };
