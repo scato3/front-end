7049c7e20f8c31aad19dab003cb40e18b98f342a
hs <tlsgustn93@naver.com>
2024-05-18 18:16:24 +0900

    fix: css에러 수정

--- src/app/profile/_component/ProfileNav.tsx
+++ src/app/profile/_component/ProfileNav.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
 import Icon_close from "../../../../public/icons/Icon_close.svg";
 import styles from "./nav.module.css";
+import { useRouter } from "next/navigation";
 
 export default function ProfileNav({ title }: { title: string }) {
+  const router = useRouter();
   return (
     <div className={styles.FilterContainer}>
       <p>{title}</p>
-      <Image src={Icon_close} width={30} height={30} alt="종료 버튼" />
+      <Image
+        src={Icon_close}
+        width={30}
+        height={30}
+        alt="종료 버튼"
+        onClick={() => {
+          router.push("./profile");
+        }}
+        className={styles.navImg}
+      ></Image>
     </div>
   );
 }
