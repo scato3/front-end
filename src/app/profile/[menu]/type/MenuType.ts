cdd27b45453ecadb2a13ca83badf380d522a9046
hs <tlsgustn93@naver.com>
2024-05-18 17:47:35 +0900

    fix: type에러 해결

---
+++ src/app/profile/[menu]/type/MenuType.ts
@@ -0,0 +1,17 @@
+interface MenuData {
+  id: number;
+  title: string;
+  status: string;
+  start_date: string;
+  end_date: string;
+  additionalInfos: string[];
+  category: string;
+  created_time: string;
+  cur_participants_num: number;
+  max_participants_num: number;
+}
+
+export interface IMenuDataProps {
+  data: MenuData[];
+  totalCount: number;
+}
