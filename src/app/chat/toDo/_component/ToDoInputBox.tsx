806e68a67a09058c521406de17b6d020010c5f76
hs <tlsgustn93@naver.com>
2024-05-28 23:35:18 +0900

    fix: ui 업데이트 변경

--- src/app/chat/toDo/_component/ToDoInputBox.tsx
+++ src/app/chat/toDo/_component/ToDoInputBox.tsx
@@ -2,28 +2,23 @@ import styles from "./component.module.css";
 import Button from "@/app/_component/button/Button";
 import useToDoStore from "../../store/useToDoStore";
 
-export default function ToDoInputBox({onClick}:{onClick:()=>void}) {
-    const { toDo, setToDo } = useToDoStore();
+export default function ToDoInputBox({ onClick }: { onClick: () => void }) {
+  const { setToDo } = useToDoStore();
 
-    const createToDo = (todo:string) => {
-        setToDo(todo);
-    };
+  const createToDo = (todo: string) => {
+    setToDo(todo);
+  };
 
-    return(
-        <div className={styles.ToDoInputContainer}>
-            <input 
-                className={styles.ToDoInput}
-                placeholder="할 일을 입력해 주세요"
-                onChange={(e) => createToDo(e.target.value as string)}
-            >
-            </input>
-            <Button 
-                size="very_small" 
-                property="confirm" 
-                onClick={onClick}
-            >
-                등록
-            </Button>
-        </div>
-    );
+  return (
+    <div className={styles.ToDoInputContainer}>
+      <input
+        className={styles.ToDoInput}
+        placeholder="할 일을 입력해 주세요"
+        onChange={(e) => createToDo(e.target.value as string)}
+      ></input>
+      <Button size="very_small" property="confirm" onClick={onClick}>
+        등록
+      </Button>
+    </div>
+  );
 }
