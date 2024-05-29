6d64d44c4484819a4844588f18b5a826095ee7f1
minjamie <minjae2246@gamil.com>
2024-05-29 13:58:04 +0900

    feat: 채팅 초대 메시지 카드 - api 오류 해결

--- src/app/api/createStudy.ts
+++ src/app/api/createStudy.ts
@@ -1,7 +1,4 @@
 import api from "@/_lib/fetcher";
-import { IUserState } from "@/hooks/useAuth";
-import getChat from "./chat/getChat";
-import postChat from "./chat/postChat";
 
 interface ISetStudyProps {
   category: string | null;
@@ -15,18 +12,7 @@ interface ISetStudyProps {
   tendency: string | null;
 }
 
-export default async function setStudy(body: ISetStudyProps, token: string, user: IUserState) {
+export default async function setStudy(body: ISetStudyProps, token: string) {
   const data = await api.post({ endpoint: `study`, body, authorization: token });
-  const isChat = await getChat(data, token);
-  if (isChat.length == 0) {
-    await postChat(
-      {
-        studyId: data.toString(),
-        pk: user.userId.toString(),
-        name: body.title,
-      },
-      token,
-    );
-  }
   return data;
 }
