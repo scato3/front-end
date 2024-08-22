e0d91699dc1c903f114daa3ca099c54e2bc60437
shuding0307 <suhyun2116@gmail.com>
2024-08-22 16:35:24 +0900

    fix errors

--- src/app/studySetting/studyMember/_component/NoMember.tsx
+++ src/app/studySetting/studyMember/_component/NoMember.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./noMember.module.css";
 import Image from "next/image";
-import Icon from "../../../../../public/icons/studyList/Mark_warning.svg";
+import Icon from "../../../../../public/icons/studyList/Mark_nothing.svg";
 
 interface NoStudyProps {
   children?: React.ReactNode;
