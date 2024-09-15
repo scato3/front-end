eea4bc0f36c963b7113f53af3e4e735e1c06dc81
hs <tlsgustn93@naver.com>
2024-09-15 20:07:51 +0900

    fix: 경로 수정

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -3,7 +3,7 @@ export { default as IconLogo } from './Icon_Logo.svg';
 export { default as IconNotification } from './Icon_notification.svg';
 export { default as IconWhite } from './Icon_WhiteBolt.svg';
 export { default as IconArrow } from './Icon_arrow.svg';
-export { default as IconBolt } from './Icon_bolt.svg';
+export { default as IconBolt } from './Icon_Bolt.svg';
 export { default as IconActiveBolt } from './Icon_active_bolt.svg';
 export { default as IconDate } from './Icon_date.svg';
 export { default as IconReset } from './Icon_reset.svg';
