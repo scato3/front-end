0128114800ee79e7d559e65e3112fdda2b4c31e9
hs <tlsgustn93@naver.com>
2024-09-16 23:54:58 +0900

    fix

--- src/app/studyInfo/page.tsx
+++ src/app/studyInfo/page.tsx
@@ -17,14 +17,14 @@ import { useModal } from '@/hooks/useModal';
 import ModalContainer from '@/component/common/modalContainer';
 import ModalPortal from '@/component/common/modalPortal';
 import JoinStudyModal from '@/component/modal/joinStudyModal';
-import { useState, Suspense } from 'react';
+import { useState } from 'react';
 import {
   usePostFavoriteStudy,
   useDeleteFavoriteStudy,
 } from '@/apis/study/favorite';
 
 export default function StudyInfo() {
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams(); // 클라이언트 측 훅
   const studyId = searchParams.get('studyId');
 
   const { data } = useGetStudyDetail(Number(studyId));
@@ -44,11 +44,6 @@ export default function StudyInfo() {
     return option ? option.label : undefined;
   }
 
-  // function getTendencyLabel(key: string): string | undefined {
-  //   const option = durationOption.find((item) => item.key === key);
-  //   return option ? option.label : undefined;
-  // }
-
   const handleJoinStudy = () => {
     mutate(Number(studyId), {
       onSuccess: (res) => {
@@ -61,7 +56,6 @@ export default function StudyInfo() {
     });
   };
 
-  // 즐겨찾기 추가 핸들러
   const handleAddFavorite = () => {
     postFavorite.mutate(Number(studyId), {
       onSuccess: () => {
@@ -73,7 +67,6 @@ export default function StudyInfo() {
     });
   };
 
-  // 즐겨찾기 삭제 핸들러
   const handleRemoveFavorite = () => {
     deleteFavorite.mutate(Number(studyId), {
       onSuccess: () => {
@@ -86,124 +79,122 @@ export default function StudyInfo() {
   };
 
   return (
-    <Suspense>
-      <div className={styles.Container}>
-        <Navigation title={data?.title} />
-        <Image src={IconHoriz} alt="horiz" className={styles.horiz} />
-        <div className={styles.separator}></div>
-        <div className={styles.section}>
-          <div className={styles.Header}>
-            <div className={styles.category}>{data?.category}</div>
-            {data?.matching_type === 'Quick' ? (
-              <div className={styles.quick}>
-                <Image src={IconWhite} alt="빠른매칭" width={11} height={14} />
-                빠른매칭
-              </div>
-            ) : null}
-          </div>
-          <div className={styles.title}>{data?.title}</div>
-          {/* \n 처리 */}
-          <div className={styles.description}>
-            {data?.description?.split('\n').map((line: string) => (
-              <span key={line}>
-                {line}
-                <br />
-              </span>
-            ))}
-          </div>
-          <div className={styles.tagsContainer}>
-            {data?.tags?.map((tag: string) => (
-              <div key={tag} className={styles.tag}>
-                # {tag}
-              </div>
-            ))}
-          </div>
-          <div className={styles.thickSeperator}></div>
-          <h2 className={styles.howToStudyHeader}>저희는 이렇게 공부해요</h2>
-          <div className={styles.howToStudyContainer}>
-            <div className={styles.duration}>
-              <div className={styles.date}>
-                <Image src={IconDate} alt="달력" width={15} height={15} />
-                <p className={styles.subText}>기간</p>
-              </div>
-              <p>
-                {koreanFormatDate(data?.start_date)} ~{' '}
-                {koreanFormatDate(data?.end_date)} (
-                {getDurationLabel(data?.duration)})
-              </p>
+    <div className={styles.Container}>
+      <Navigation title={data?.title} />
+      <Image src={IconHoriz} alt="horiz" className={styles.horiz} />
+      <div className={styles.separator}></div>
+      <div className={styles.section}>
+        <div className={styles.Header}>
+          <div className={styles.category}>{data?.category}</div>
+          {data?.matching_type === 'Quick' ? (
+            <div className={styles.quick}>
+              <Image src={IconWhite} alt="빠른매칭" width={11} height={14} />
+              빠른매칭
             </div>
-            <div className={styles.person}>
-              <div className={styles.date}>
-                <Image src={IconPeople} alt="사람" width={15} height={15} />
-                <p className={styles.subText}>인원</p>
-              </div>
-              <p>
-                {data?.membersList?.length}명 참여중 (
-                {data?.membersList?.length}/{data?.max_participants_num})
-              </p>
+          ) : null}
+        </div>
+        <div className={styles.title}>{data?.title}</div>
+        {/* \n 처리 */}
+        <div className={styles.description}>
+          {data?.description?.split('\n').map((line: string) => (
+            <span key={line}>
+              {line}
+              <br />
+            </span>
+          ))}
+        </div>
+        <div className={styles.tagsContainer}>
+          {data?.tags?.map((tag: string) => (
+            <div key={tag} className={styles.tag}>
+              # {tag}
             </div>
-            <div className={styles.tendency}>
-              <div className={styles.date}>
-                <Image src={IconBook} alt="책" width={15} height={15} />
-                <p className={styles.subText}>성향</p>
-              </div>
-              <p>{data?.tendency}</p>
+          ))}
+        </div>
+        <div className={styles.thickSeperator}></div>
+        <h2 className={styles.howToStudyHeader}>저희는 이렇게 공부해요</h2>
+        <div className={styles.howToStudyContainer}>
+          <div className={styles.duration}>
+            <div className={styles.date}>
+              <Image src={IconDate} alt="달력" width={15} height={15} />
+              <p className={styles.subText}>기간</p>
             </div>
-            <h2 className={styles.joinMember}>참여 멤버</h2>
-            <div className={styles.memberContainer}>
-              {data?.membersList?.map(
-                (member: { nickname: string; _owner: boolean }) => (
-                  <div className={styles.memberBox} key={member.nickname}>
-                    <div className={styles.memberCircle}>
-                      {member._owner === true ? (
-                        <Image
-                          src={IconOwner}
-                          width={22}
-                          height={22}
-                          alt="방장 아이콘"
-                          className={styles.owner}
-                        />
-                      ) : null}
-                    </div>
-                    <p className={styles.nickname}>{member.nickname}</p>
+            <p>
+              {koreanFormatDate(data?.start_date)} ~{' '}
+              {koreanFormatDate(data?.end_date)} (
+              {getDurationLabel(data?.duration)})
+            </p>
+          </div>
+          <div className={styles.person}>
+            <div className={styles.date}>
+              <Image src={IconPeople} alt="사람" width={15} height={15} />
+              <p className={styles.subText}>인원</p>
+            </div>
+            <p>
+              {data?.membersList?.length}명 참여중 ({data?.membersList?.length}/
+              {data?.max_participants_num})
+            </p>
+          </div>
+          <div className={styles.tendency}>
+            <div className={styles.date}>
+              <Image src={IconBook} alt="책" width={15} height={15} />
+              <p className={styles.subText}>성향</p>
+            </div>
+            <p>{data?.tendency}</p>
+          </div>
+          <h2 className={styles.joinMember}>참여 멤버</h2>
+          <div className={styles.memberContainer}>
+            {data?.membersList?.map(
+              (member: { nickname: string; _owner: boolean }) => (
+                <div className={styles.memberBox} key={member.nickname}>
+                  <div className={styles.memberCircle}>
+                    {member._owner === true ? (
+                      <Image
+                        src={IconOwner}
+                        width={22}
+                        height={22}
+                        alt="방장 아이콘"
+                        className={styles.owner}
+                      />
+                    ) : null}
                   </div>
-                )
-              )}
-            </div>
+                  <p className={styles.nickname}>{member.nickname}</p>
+                </div>
+              )
+            )}
           </div>
         </div>
-        <div className={styles.bottomContainer}>
-          {isFavorite ? (
-            <Image
-              src={IconActiveHeart}
-              alt="active heart"
-              className={styles.heart}
-              width={39}
-              height={39}
-              onClick={handleRemoveFavorite}
-            />
-          ) : (
-            <Image
-              src={IconHeart}
-              alt="heart"
-              className={styles.heart}
-              width={39}
-              height={39}
-              onClick={handleAddFavorite}
-            />
-          )}
-          <Button size="medium" onClick={handleJoinStudy}>
-            스터디 가입하기
-          </Button>
-        </div>
-        {openModal && (
-          <ModalPortal>
-            <ModalContainer handleCloseModal={handleCloseModal}>
-              <JoinStudyModal message={message} />
-            </ModalContainer>
-          </ModalPortal>
-        )}
       </div>
-    </Suspense>
+      <div className={styles.bottomContainer}>
+        {isFavorite ? (
+          <Image
+            src={IconActiveHeart}
+            alt="active heart"
+            className={styles.heart}
+            width={39}
+            height={39}
+            onClick={handleRemoveFavorite}
+          />
+        ) : (
+          <Image
+            src={IconHeart}
+            alt="heart"
+            className={styles.heart}
+            width={39}
+            height={39}
+            onClick={handleAddFavorite}
+          />
+        )}
+        <Button size="medium" onClick={handleJoinStudy}>
+          스터디 가입하기
+        </Button>
+      </div>
+      {openModal && (
+        <ModalPortal>
+          <ModalContainer handleCloseModal={handleCloseModal}>
+            <JoinStudyModal message={message} />
+          </ModalContainer>
+        </ModalPortal>
+      )}
+    </div>
   );
 }
