3f1bccfa347637a2e0c820eff8283a77a0d146a4
hs <tlsgustn93@naver.com>
2024-09-17 00:03:25 +0900

    fix: suspense 추가

--- src/app/studyInfo/page.tsx
+++ src/app/studyInfo/page.tsx
@@ -3,14 +3,17 @@ import Navigation from '@/component/common/navigation';
 import Image from 'next/image';
 import styles from './studyInfo.module.scss';
 import { IconHoriz } from '../../../public/icons';
+import { Suspense } from 'react';
 
 export default function StudyInfo() {
   return (
-    <div className={styles.Container}>
-      <Navigation title="Study Info" />
-      <Image src={IconHoriz} alt="horiz" className={styles.horiz} />
-      <div className={styles.separator}></div>
-      <StudyInfoClient />
-    </div>
+    <Suspense>
+      <div className={styles.Container}>
+        <Navigation title="Study Info" />
+        <Image src={IconHoriz} alt="horiz" className={styles.horiz} />
+        <div className={styles.separator}></div>
+        <StudyInfoClient />
+      </div>
+    </Suspense>
   );
 }
