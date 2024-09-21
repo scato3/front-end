5d216972ba9251861b326983d05ae4dae50d7892
hs <tlsgustn93@naver.com>
2024-09-21 12:32:36 +0900

    fix: suspense 제거

--- src/app/studySetting/edit/page.tsx
+++ src/app/studySetting/edit/page.tsx
@@ -1,16 +1,13 @@
 import styles from './edit.module.scss';
 import Navigation from '@/component/common/navigation';
 import StudyEditClient from './studyEditClient';
-import { Suspense } from 'react';
 
 export default function StudyEdit() {
   return (
     <div className={styles.Container}>
       <Navigation title="스터디 수정" />
       <div className={styles.verticalLine}></div>
-      <Suspense>
-        <StudyEditClient />
-      </Suspense>
+      <StudyEditClient />
     </div>
   );
 }
