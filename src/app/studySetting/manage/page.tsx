0fb666a99b051dc15083ce5b58b6851b3755a4f4
hs <tlsgustn93@naver.com>
2024-09-21 12:39:16 +0900

    fix: 에러 수정

--- src/app/studySetting/manage/page.tsx
+++ src/app/studySetting/manage/page.tsx
@@ -1,11 +1,13 @@
 import Navigation from '@/component/common/navigation';
 import styles from './manage.module.scss';
+import StudyManageClient from './studyManageClient';
 
 export default function StudyManage() {
   return (
     <div className={styles.Container}>
       <Navigation title="스터디 멤버 관리" />
       <div className={styles.verticalLine}></div>
+      <StudyManageClient />
     </div>
   );
 }
