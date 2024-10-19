40374c09d69c4a1bd4d97740a8d28437df9ce8ac
scato3 <tlsgustn93@naver.com>
2024-10-19 03:00:40 +0000

    fix: Suspense 감싸기

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,13 @@
 import styles from './chat.module.scss';
 import ChatClient from './chatClient';
+import { Suspense } from 'react';
 
 export default function Chat() {
   return (
-    <div className={styles.Container}>
-      <ChatClient />
-    </div>
+    <Suspense>
+      <div className={styles.Container}>
+        <ChatClient />
+      </div>
+    </Suspense>
   );
 }
